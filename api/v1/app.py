30ad430aeab24b3ac4094d5bb62a0b9e25474974
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-05-01 17:00:02 +0100

    Solve task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,4 +27,4 @@ def error_404(error):
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(host=HOST, port=PORT, threaded=True, debug=True)
