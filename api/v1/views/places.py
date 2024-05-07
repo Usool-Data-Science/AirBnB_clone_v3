e11e7fee147dec859e23c5fd9386aee79a9cc43f
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-05-07 10:56:54 +0100

    Update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,7 +3,7 @@
 from flask import jsonify, abort, request
 from models import storage
 from models.user import User
-from models.amentiy import Amenity
+from models.amenity import Amenity
 from models.state import State
 from models.city import City
 from models.place import Place
